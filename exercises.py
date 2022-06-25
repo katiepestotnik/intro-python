1f9395cc1e07ff3b2251430760747362fc3ab88a
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-06-25 15:08:37 -0600

    wednesday work

--- exercises.py
+++ exercises.py
@@ -1,15 +1,21 @@
 # Branching & Looping Exercises
 
 color = input('Enter "green", "yellow", "red": ').lower()
-print(f'The user entered {color}')
+#print(f'The user entered {color}')
 
 # Write the if...elif...else statement as described in the lesson
+while color != 'quit':
+    if color == 'green':
+        print('Go!!!')
+    elif color == 'yellow':
+        print('Slow Down')
+    elif color == 'red':
+        print('Stop or get a ticket')
+    elif color == 'quit':
+        print('user quit game')
+        break
+    else:
+        print('Not a traffic color light')
+    color = input('Enter "green", "yellow", "red": ').lower()
+    
 
-if color == 'green':
-    print('Go!!!')
-elif color == 'yellow':
-    print('Slow Down')
-elif color == 'red':
-    print('Stop or get a ticket')
-else:
-    print('Not a traffic color light')
--- python-control-flow.py
+++ python-control-flow.py
@@ -11,3 +11,42 @@ elif walls == 'dusty':
     print('dust them')
 else:
     print('nice clean walls')
+    
+
+#looping
+#for
+colors = ['green', 'yellow', 'red']
+for color in colors:
+    print(color)
+#while
+# while color != 'quit':
+#     if color == 'green':
+#         print('Go!!!')
+#     elif color == 'yellow':
+#         print('Slow Down')
+#     elif color == 'red':
+#         print('Stop or get a ticket')
+#     elif color == 'quit':
+#         print('user quit game')
+#         break
+#     else:
+#         print('Not a traffic color light')
+#     color = input('Enter "green", "yellow", "red": ').lower()
+    
+# ranges
+for number in range(6):
+    print(number)
+
+for num in range(2, 10, 2):
+    print(num)
+
+#list
+nums = list(range(10))
+print(nums)
+#tuple
+tuples_nums = tuple(range(10))
+print(tuples_nums)
+
+#range negative step
+for num in range(10, -5, -1):
+    print(num)
